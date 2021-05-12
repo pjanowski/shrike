7af50cfa7e3bf69763ce6faefdcda7b45897e8fa
fuhuifang <32152743+fuhuifang@users.noreply.github.com>
2021-05-12 12:19:23 -0700

    fix shrike.pipeline module not found (#9)

--- shrike/__init__.py
+++ shrike/__init__.py
@@ -6,4 +6,5 @@ Python utilities to aid "compliant experimentation" - training
 machine learning models without seeing the training data.
 """
 
-__version__ = "1.0.0-rc2"
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+__version__ = "1.0.0-rc3"
